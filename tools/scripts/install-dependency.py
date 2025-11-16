a25204f1aae5ef61858080461e2a6c9053b5f281
NuhilLucas <nuhillucasmail@163.com>
2025-11-16 11:35:17 +0800

    添加一键安装环境脚本

--- tools/scripts/install-dependency.py
+++ tools/scripts/install-dependency.py
@@ -1,4 +1,4 @@
-# python -m nuitka --onefile --enable-plugin=tk-inter --output-dir=dist tools/scripts/install_dependency.py
+# python -m nuitka --onefile --enable-plugin=tk-inter --output-dir=dist tools/scripts/install-dependency.py
 
 from os.path import exists as osp_exists
 from subprocess import run as sp_run
